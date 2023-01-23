9cf3654166c56f5ab4379381e55460c9df67df4f
Alessio Limina <alessio.limina@gmail.com>
2023-01-23 21:39:53 +0100

    Exercise fixing

--- src/it/develhope/encapsulation/Start.java
+++ src/it/develhope/encapsulation/Start.java
@@ -10,21 +10,25 @@ public class Start {
         Person person = new Person();
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Type the name: ");
         person.setName(scanner.nextLine());
         System.out.println("Her name is " + person.getName());
 
         System.out.println("----------------------------------");
 
+        System.out.println("Type the surname: ");
         person.setSurname(scanner.nextLine());
         System.out.println("Her surname is " + person.getSurname());
 
         System.out.println("----------------------------------");
 
+        System.out.println("Type the height: ");
         person.setHeight(scanner.nextDouble());
         System.out.println("Her height is " + person.getHeight());
 
         System.out.println("----------------------------------");
 
+        System.out.println("Type the age: ");
         person.setAge(scanner.nextInt());
         System.out.println("Her age is " + person.getAge());
 
