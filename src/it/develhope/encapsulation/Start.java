7adc4fa25ad09fe59dbfd993c28770f3c98f1ee5
Alessio Limina <alessio.limina@gmail.com>
2023-01-23 17:32:29 +0100

    Exercise fixing

--- src/it/develhope/encapsulation/Start.java
+++ src/it/develhope/encapsulation/Start.java
@@ -1,28 +1,31 @@
 package it.develhope.encapsulation;
 
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
 
         System.out.println("-------Creating a new person------");
 
         Person person = new Person();
+        Scanner scanner = new Scanner(System.in);
 
-        person.setName("Monica");
+        person.setName(scanner.nextLine());
         System.out.println("Her name is " + person.getName());
 
         System.out.println("----------------------------------");
 
-        person.setSurname("Poli");
+        person.setSurname(scanner.nextLine());
         System.out.println("Her surname is " + person.getSurname());
 
         System.out.println("----------------------------------");
 
-        person.setHeight(165.57);
+        person.setHeight(scanner.nextDouble());
         System.out.println("Her height is " + person.getHeight());
 
         System.out.println("----------------------------------");
 
-        person.setAge(24);
+        person.setAge(scanner.nextInt());
         System.out.println("Her age is " + person.getAge());
 
         System.out.println("----------------------------------");
